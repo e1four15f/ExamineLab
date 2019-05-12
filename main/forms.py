4a70aee2edc7561d4c20c7e17c709d7bc0f7b7b6
e1four15f <Tesskyrim000@gmail.com>
2019-05-12 14:42:55 +0300

    Changed the language in forms and added homepage

--- main/forms.py
+++ main/forms.py
@@ -1,17 +1,38 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import password_validation
 from .models import User
 from djangocodemirror.fields import CodeMirrorField
 
 
 
 class NewUserForm(UserCreationForm):
+    name = forms.CharField(max_length=50, required=False,
+                                label='Имя')
+    surname = forms.CharField(max_length=50, required=False, 
+                                label='Фамилия')
     email = forms.EmailField(required=True)
-
+    password1 = forms.CharField(
+        label='Пароль',
+        strip=False,
+        widget=forms.PasswordInput,
+        help_text='\
+        1) Ваш пароль не может быть слишком похож на другую вашу личную информацию.<br/>\
+        2) Ваш пароль должен содержать не менее 8 символов.<br/>\
+        3) Ваш пароль не может быть часто используемым паролем.<br/>\
+        4) Ваш пароль не может быть полностью цифровым.',
+    )
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text='Повторите свой пароль'
+    )
+    
     class Meta:
         model = User
-        fields = ('email', 'password1', 'password2')
-
+        fields = ('name', 'surname', 'email', 'password1', 'password2')
+        
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
         user.username = self.cleaned_data['email']
@@ -19,7 +40,6 @@ class NewUserForm(UserCreationForm):
         if commit:
             user.save()
         return user
-
 
 
 class SubmitForm(forms.Form):
@@ -31,20 +51,26 @@ class SubmitForm(forms.Form):
 
 
 class AddCourseForm(forms.Form):
+    # TODO получать поля из бд
     title_size = 50
     summary_size = 400
-    title = forms.CharField(max_length=title_size, required=True)
-    summary = forms.CharField(max_length=summary_size)
+    title = forms.CharField(max_length=title_size, required=True,
+                                label='Название')
+    summary = forms.CharField(max_length=summary_size,
+                                label='Описание')
 
     class Meta:
         fields = ('title', 'summary')
 
 
 class AddTaskForm(forms.Form):
+    # TODO получать поля из бд
     title_size = 50
     summary_size = 400
-    title = forms.CharField(max_length=title_size, required=True)
-    summary = forms.CharField(max_length=summary_size)
+    title = forms.CharField(max_length=title_size, required=True, 
+                                label='Название')
+    summary = forms.CharField(max_length=summary_size,
+                                label='Описание')
     # TODO Поля для добавление Test-ов
     #rating = forms.Ch
 
